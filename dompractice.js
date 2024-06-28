4a48ea111d3ff31b87eaea2417c9a595613c2950
Siddharth Kumar <165459065+Siddharth-kumar-yadav@users.noreply.github.com>
2024-06-28 13:44:15 +0530

    dom js code

---
+++ dompractice.js
@@ -0,0 +1,22 @@
+// let h2=document.querySelector("h2")
+
+// console.dir(h2.innerText);
+// h2.innerText=h2.innerText+" by Siddharth";
+
+//access div.
+// let divs=document.querySelectorAll(".box");
+// console.log(divs[0]);
+// divs[0].innerText= "My first div with new unique value";
+// divs[1].innerText="My secons div with new unique values";
+
+//changing text with the help of for loop.
+// let divs=document.querySelectorAll(".box");
+// let idx=1;
+// for(let div of divs){
+//     div.innerText=`New unique value ${idx}`
+//     idx++;
+// }
+  
+
+// dom part 2.
+
